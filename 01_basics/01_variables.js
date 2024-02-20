a360f5e2a79c82fd215acadaf8a9f3dac6298f7d
SleeplessTribe <155296322+SleeplessTribe@users.noreply.github.com>
2024-02-20 09:20:16 +0000

    add notes for variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "rashmikant@mail.com"
+var accountPassword = "12345"
+//Note : Prefer not use var(keyword to asign a varibale) 
+//because of issue in block scope and functional scope
+accountCity = "Navsari"
+
+//accountId  = 3 //not allowed runtime error
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]) 
