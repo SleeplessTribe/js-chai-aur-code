9c15ae33a5cfde749d1ed24023c673538668f4fa
SleeplessTribe <155296322+SleeplessTribe@users.noreply.github.com>
2024-02-28 07:09:24 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,34 @@
+//console.log(2 > 1); //true
+//console.log(2 >= 1); //true
+//console.log(2 < 1); //false
+//console.log(2 <= 1); //false
+//console.log(2 ==1); //false
+//console.log(2 !=1); //true
+
+//######################################
+//console.log("2" > 1); //true
+//console.log("02" > 1); //true
+//console.log("01" > 2); //false
+//console.log("03" < 2); //false
+//console.log(1 > "02"); //false
+//**JS automatic convert string value type into number 
+//when comparision perform
+
+//#####################################
+//console.log(null > 0); //false
+//console.log(null < 0); //false
+//console.log(null == 0); //false
+//console.log(null >= 0); //true
+//console.log(null <= 0); //true
+//console.log(null != 0); //true
+
+//** The reason is that an equality check == and 
+//comparisions > < >= <= != work diffrently.
+//Comparisions convert null to a number, treating it as 0.
+//that's why (1-3) > < == is false and (4-6) >= <= != is true.
+
+
+
+
+
+
