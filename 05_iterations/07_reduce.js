9e6bb60806d63070c1a8cdcd8109044e2b3c183f
SleeplessTribe <155296322+SleeplessTribe@users.noreply.github.com>
2024-03-03 19:05:18 +0000

    reduce

---
+++ 05_iterations/07_reduce.js
@@ -0,0 +1,35 @@
+const myNums = [1, 2, 3, 4]
+
+/*const total = myNums.reduce( function (accumulator, currentValue) {
+    console.log(`acc: ${accumulator} and curvalue : ${currentValue}`)
+   return  accumulator + currentValue
+}, 3)*/
+
+/*const total  = myNums.reduce((acc, curr) => acc + curr, 0);
+console.log(total)*/
+
+/*const shoppingCart = [
+    {
+        itemName : "JS course",
+        price : 2999
+    },
+    {
+        itemName : "JS course",
+        price : 2999
+    },
+    {
+        itemName : "py course",
+        price : 999
+    },
+    {
+        itemName : "mobile dev course",
+        price : 5999
+    },
+    {
+        itemName : "data science course",
+        price : 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+console.log(priceToPay)*/
