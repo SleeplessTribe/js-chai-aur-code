a748e445f2ddb8579e9a7779dff25334074a8bc1
SleeplessTribe <155296322+SleeplessTribe@users.noreply.github.com>
2024-03-03 16:44:44 +0000

    while -do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+//while loop -> Entry control
+/*
+let index = 0
+while(index <= 10){
+    console.log(`value of index is ${index}`);
+    index = index + 2
+}
+*/
+let myHeroes = ['flash', 'batman', 'superman']
+let arr = 0
+/*while(arr < myHeroes.length){
+    console.log(`value is ${myHeroes[arr]}`);
+    arr = arr + 1
+}
+*/
+
+
+//do...while loop -> Exit control (atlist one output)
+let score = 11 
+/*
+do{
+    console.log(`Score is ${score}`)
+    score++
+}while(score <= 10);
+*/
+
