8d8ae32f7b8889b4c6e8b94e66043576e2fd3164
SleeplessTribe <155296322+SleeplessTribe@users.noreply.github.com>
2024-03-03 15:10:30 +0000

    truthy - nullish coalescing operator

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,33 @@
+//const email = 'r@mail.ai' //have an email
+//const email = "" //don't have an email
+//const email = [] //have an email
+
+// if(email){
+//     console.log('have an email');
+// }else{   
+//      console.log('don\'t have an email');
+// }
+
+/*
+******* Falsy values **************
+    false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+******* Truthy values **************
+    true, 'false', '0', " ", [], {}, function(){}
+*/
+
+//Nullish Coalescing Operator (??) : null undefined
+
+let val;
+//val = 5 ?? 10
+//console.log(val);//5
+
+//val = null ?? 10
+//console.log(val);//10
+
+//val = undefined ?? 10
+//console.log(val);//10
+
+//val = null ?? 10 ?? 20
+//console.log(val); //10
+
